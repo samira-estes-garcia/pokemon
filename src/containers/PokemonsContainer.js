0dbc88dc1ed69d4fce93e3af6cadf2851b39e366
samira-estes-garcia <samira.estes.garcia@gmail.com>
2021-02-05 12:31:31 -0600

    added info to pokemon component

--- src/containers/PokemonsContainer.js
+++ src/containers/PokemonsContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
 import { GET_POKEMONS } from '../graphql/get-pokemons'
+import { Pokemon } from '../components/Pokemon';
 
 export const PokemonsContainer = () => {
     const { data: { pokemons = [] } = {} } = useQuery(GET_POKEMONS, {
@@ -9,7 +10,7 @@ export const PokemonsContainer = () => {
 
     return (
         <div className="pokemons">
-            {pokemons && pokemons.map(pokemon => JSON.stringify(pokemon, null, 2))}
+            {pokemons && pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
         </div>
     )
 }
